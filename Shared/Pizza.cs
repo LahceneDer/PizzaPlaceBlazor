4bf6e7fdec5324693c6ccefb0c262d117a7f9936
LahceneDer <l.dergham@esi-sba.dz>
2022-03-31 14:07:06 +0100

    initial commit

---
+++ Shared/Pizza.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaPlace.Shared
+{
+    
+    public enum Spiciness
+    {
+        None,
+        Spicy,
+        Hot
+    }
+    public class Pizza
+    {
+        public Pizza(int id, string name)
+        {
+            
+        }
+    }
+}
